bc98f41cd2c6006f7268d801d2c99ed933345526
Paco <127772244+pacojf93@users.noreply.github.com>
2025-01-06 15:25:31 +0100

    refactored board and display elements into module patterns

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-
 const game = (function (doc) {
+    const gameBoard = (function(){
+        const board = Array(9).fill("")
+        const getBoard = () => board
+        const resetBoard = () => board.forEach((p, i, a) => a[i] = "")
+        const markPosition = (index, icon) => board[index] = icon
+        const isBoardFull = () => board.every( p => p !== "")
+        const checkForLine = function (icon) {
+            const lines = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+            ]
+            
+            let isVictory = false
+            lines.forEach((line) => {
+                if(line.map(index => board[index])
+                    .every(index => index === icon)) {
+                        isVictory = true
+                        return
+                }
+            })    
+            return isVictory
+        }
 
-    const printGameBoard = () => doc.querySelectorAll(".board-position").forEach((position) => position.textContent = gameBoard[position.id])
-    const printCurrentPlayer = () => doc.querySelector("#current-player").textContent = `${currentPlayer.getIcon()}'s turn`
-    const renderDisplay = () => {
-        printGameBoard()
-        printCurrentPlayer()
-    }
+        return {resetBoard, markPosition, isBoardFull, getBoard, checkForLine}
+    })()
 
-    const resetBoard = () => gameBoard.forEach((p, i, a) => a[i] = "")
-
-    const markPosition = function (index, icon) {
-        gameBoard[index] = icon
-    }
-
-    const checkForLine = function (icon) {
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
+    const display = (function (doc) {
+        const printGameBoard = () => doc.querySelectorAll(".board-position").forEach((position) => position.textContent = gameBoard.getBoard()[position.id])
+        const printCurrentPlayer = () => doc.querySelector("#current-player").textContent = `${currentPlayer.getIcon()}'s turn`
+        const renderDisplay = () => {
+            printGameBoard()
+            printCurrentPlayer()
+        }
         
-        let isVictory = false
+        return {printGameBoard, printCurrentPlayer, renderDisplay}
+    })(doc)
 
-        lines.forEach((line) => {
-            if(line.map(index => gameBoard[index])
-                .every(index => index === icon)) {
-                    isVictory = true
-                    return
-            }
-        })
-
-        return isVictory
-    }
-
-    const isBoardFull = function () {
-        return gameBoard.every( p => p !== "")
-    }
 
     const createPlayer = function (icon) {
         const playerIcon = icon 
@@ -58,37 +58,36 @@ const game = (function (doc) {
     const whoseTurn = () => console.log(currentPlayer.icon)
 
     const playRound = function (position) {
-        if(gameBoard[position] === "") {
+        if(gameBoard.getBoard()[position] === "") {
 
-            markPosition(position, currentPlayer.getIcon())
-            printGameBoard()
+            gameBoard.markPosition(position, currentPlayer.getIcon())
+            display.printGameBoard()
 
-            if(checkForLine(currentPlayer.getIcon())) {
+            if(gameBoard.checkForLine(currentPlayer.getIcon())) {
                 alert(`${currentPlayer.getIcon()} wins!`)
-                resetBoard()
-                printGameBoard()
-            } else if (isBoardFull()) {
+                gameBoard.resetBoard()
+                display.printGameBoard()
+            } else if (gameBoard.isBoardFull()) {
                 alert("draw!")
-                resetBoard()
-                printGameBoard()
+                gameBoard.resetBoard()
+                display.printGameBoard()
             }
 
             togglePlayer()
-            printCurrentPlayer()
+            display.printCurrentPlayer()
 
         }
     }
 
     // initialize game
-    const gameBoard = Array(9).fill("")
     const playerA = createPlayer("x")
     const playerB = createPlayer("o")
     let currentPlayer = playerA
 
-    return {renderDisplay, markPosition, checkForLine, isBoardFull, whoseTurn, playRound, playerA, playerB, currentPlayer}
+    return {display, gameBoard, whoseTurn, playRound, playerA, playerB, currentPlayer}
 })(document)
 
-game.renderDisplay()
+game.display.renderDisplay()
 
 document.querySelector("#board-container").addEventListener("click", (event) => {
     const position = event.target.id
